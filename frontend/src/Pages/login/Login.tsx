f0fd7027fb9d7df3a9749dc800c7b96afec529c5
Lucas Nery <lucas.leite1@br.nestle.com>
2024-06-05 12:06:38 -0300

    update login

--- frontend/src/Pages/login/Login.tsx
+++ frontend/src/Pages/login/Login.tsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import { useState } from "react"
+import HeaderLoggedOut from "../../components/HeaderLoggedOut/HeaderLoggedOut"
+import InputField from "../../components/InputField/InputField"
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
   return (
-    <h1>Login</h1>
+    <>
+      <HeaderLoggedOut />
+      <div className="introduction">
+        <div className="introduction__container">
+          <h1 className="introduction__title">controlaGastos</h1>
+          <p className="introduction__subtitle">Um webapp para você conseguir fazer o controle dos seus gastos e entender para onde seu dinheiro esta indo</p>
+        </div>
+
+        <div>
+          <h1>Acesse sua conta</h1>
+
+          <form action="">
+            <InputField
+              label="Digite seu e-mail" 
+              name="mail"
+              type="email"
+              placeholder="Seu e-mail"
+              value={email}
+              onChange={({ currentTarget }) => setEmail(currentTarget.value)}
+            />
+
+            <InputField
+              label="Digite sua senha" 
+              name="mail"
+              type="password"
+              placeholder="Sua senha"
+              value={password}
+              onChange={({ currentTarget }) => setPassword(currentTarget.value)}
+            />
+
+            <button 
+              type="submit" 
+              className="btn-primary-custom" 
+              style={{ marginTop: "24px" }}
+            >
+              Entrar
+            </button>
+
+            <a href="/create-account" className="btn-secondary-custom">Criar uma conta</a>
+          </form>
+
+        </div>
+      </div>
+    </>
   )
 }
 
