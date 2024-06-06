d585d8ec35582bdcdaadd6ea9c2dc1bbca1f322d
Lucas Nery <lucas.leite1@br.nestle.com>
2024-06-06 08:56:26 -0300

    create account

--- frontend/src/Pages/createAccount/CreateAccount.tsx
+++ frontend/src/Pages/createAccount/CreateAccount.tsx
@@ -1,20 +1,78 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 
 import "./CreateAccount.scss"
 
 import HeaderLoggedOut from '../../components/HeaderLoggedOut/HeaderLoggedOut'
 import InputField from '../../components/InputField/InputField'
+import { useNavigate } from 'react-router-dom'
+import Toast from '../../components/Toast/Toast'
+import { formatCurrency } from '../../utils/FormatValue'
 
 const CreateAccount = () => {
+  const navigate = useNavigate()
+
   const [email, setEmail] = useState("")
   const [name, setName] = useState("")
-  const [salary, setSalary] = useState("")
+  const [salary, setSalary] = useState("0")
   const [password, setPassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("")
+  const [errors, setErrors] = useState<string[]>([]);
+  const [message, setMessage] = useState("")
+  const [error, setError] = useState(false)
+
+  const handleCreateAccount = (e: FormEvent) => {
+    e.preventDefault()
+    const btnSubmit = document.querySelector(".btn-primary-custom") as HTMLButtonElement;
+    btnSubmit.disabled = true;
+
+    const newErrors: string[] = [];
+
+    if (!email) newErrors.push("email")
+    if (!name) newErrors.push("name")
+    if (!salary) newErrors.push("salary")
+    if (!password) newErrors.push("password")
+    if (!confirmPassword) newErrors.push("confirmPassword")
+    if(confirmPassword !== password) newErrors.push("confirmPassword")
+
+    setErrors(newErrors);
+
+    if (newErrors.length) {
+      btnSubmit.disabled = false;
+      return
+    }
+
+    fetch(`${import.meta.env.VITE_DEFAULT_URL}/register`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ 
+        email, 
+        password, 
+        salary,
+        name,
+        username: `${name}${email}` 
+      }) 
+    })
+    .then(response => response.json())
+    .then(data => {
+      setMessage(data.message)
+      setTimeout(() => navigate("/login"), 2000);
+    })
+    .catch(error => {
+      btnSubmit.disabled = true;
+      setError(true)
+      setMessage(error)
+      setTimeout(() => navigate("/login"), 2000);
+    });
+  }
   
   return (
     <>
       <HeaderLoggedOut />
+
+      {message && <Toast message={message} error={error}/>}
+
       <div className="introduction">
         <div className="introduction__container">
           <h1 className="introduction__title">controlaGastos</h1>
@@ -24,7 +82,7 @@ const CreateAccount = () => {
         <div>
           <h1>Crie sua conta</h1>
 
-          <form action="">
+          <form onSubmit={handleCreateAccount}>
             <InputField
               label="E-mail" 
               name="mail"
@@ -32,6 +90,7 @@ const CreateAccount = () => {
               placeholder="Seu e-mail"
               value={email}
               onChange={({ currentTarget }) => setEmail(currentTarget.value)}
+              errorMessage={errors.includes("email") ?  "E-mail não pode ser vazia!" : ""}
             />
 
             <InputField
@@ -41,6 +100,7 @@ const CreateAccount = () => {
               placeholder="Seu nome"
               value={name}
               onChange={({ currentTarget }) => setName(currentTarget.value)}
+              errorMessage={errors.includes("name") ?  "Nome não pode ser vazio!" : ""}
             />
 
             <InputField
@@ -48,8 +108,9 @@ const CreateAccount = () => {
               name="salary"
               type="text"
               placeholder="Seu salário"
-              value={salary}
-              onChange={({ currentTarget }) => setSalary(currentTarget.value)}
+              value={formatCurrency(salary)}
+              onChange={({ currentTarget }) => setSalary(formatCurrency(currentTarget.value))}
+              errorMessage={errors.includes("salary") ?  "Salário não pode ser vazio!" : ""}
             />
 
             <InputField
@@ -59,6 +120,7 @@ const CreateAccount = () => {
               placeholder="Sua senha"
               value={password}
               onChange={({ currentTarget }) => setPassword(currentTarget.value)}
+              errorMessage={errors.includes("password") ?  "Senha não pode ser vazia!" : ""}
             />
 
             <InputField
@@ -68,6 +130,7 @@ const CreateAccount = () => {
               placeholder="Digite novamente a senha"
               value={confirmPassword}
               onChange={({ currentTarget }) => setConfirmPassword(currentTarget.value)}
+              errorMessage={errors.includes("confirmPassword") ?  "Senha diferente ou vazia!" : ""}
             />
 
             <button 
