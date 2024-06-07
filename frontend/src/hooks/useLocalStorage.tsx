df493b10aa44a664a82a56b312ec00c5cea73e71
Lucas Nery <lucas.leite1@br.nestle.com>
2024-06-07 13:31:34 -0300

    update style select and add style modal

---
+++ frontend/src/hooks/useLocalStorage.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useLocalStorage = (key: string, initial: string): [string, React.Dispatch<React.SetStateAction<string>>] => {
+  const [state, setState] = useState(() => {
+    const local = window.localStorage.getItem(key)
+
+    return local ? local : initial
+  })
+
+  useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [state, key])
+
+  return [state, setState]
+}
+
+export default useLocalStorage;
