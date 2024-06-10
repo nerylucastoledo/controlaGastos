6b777cb4b8765d1b7bbbe191fa74858e5277d44c
Lucas Nery <lucas.leite1@br.nestle.com>
2024-06-10 13:31:33 -0300

    update home

--- frontend/src/Pages/dashboard/Dashboard.tsx
+++ frontend/src/Pages/dashboard/Dashboard.tsx
@@ -3,6 +3,10 @@ import "./Dashboards.scss"
 import { Header} from "../../components/Header/Header"
 import DateFilter from "../../components/DateFilter/DateFilter"
 import { useState } from "react"
+import Statistics from "../../components/Statistics/Statistics"
+import Wallet from "../../components/Wallet/Wallet"
+import Cards from "../../components/Cards/Cards"
+import RecentExpenses from "../../components/RecentExpenses/RecentExpenses"
 
 const Dashboard = () => {
   const [month, setMonth] = useState("")
@@ -12,12 +16,21 @@ const Dashboard = () => {
     <div className="dashboard">
       <Header />
       <div className="dashboard__container">
-        <div className="dashboard__container-info">
-          <h1>Olá, <strong>Lucas</strong></h1>
-          <p>Gerencie seus gastos de forma simples e eficiente.</p>
-        </div>
+        <div className="dashboard__container-content">
+          <div className="dashboard__container-content-info">
+            <h1>Olá, <strong>Lucas</strong></h1>
+            <p>Gerencie seus gastos de forma simples e eficiente.</p>
+          </div>
 
-        <DateFilter setMonth={setMonth} setYear={setYear} />
+          <DateFilter setMonth={setMonth} setYear={setYear} />
+          <div>
+            <Statistics />
+            <Wallet />
+          </div>
+
+          <Cards />
+          <RecentExpenses />
+        </div>
       </div>
     </div>
   )
