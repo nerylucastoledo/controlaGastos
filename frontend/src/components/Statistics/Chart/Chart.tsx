1f501413b40b6526da1e2a98427f2067be7cb23c
Lucas Nery <lucas.leite1@br.nestle.com>
2024-06-10 13:31:20 -0300

    add chart

---
+++ frontend/src/components/Statistics/Chart/Chart.tsx
@@ -0,0 +1,55 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const Chart = () => {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+  
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [7, 4, 5, 1, 2, 3, 6],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+    ],
+  };
+
+  return (
+    <div className='chart'>
+      <Bar options={options} data={data} />
+    </div>
+  )
+}
+
+export default Chart
