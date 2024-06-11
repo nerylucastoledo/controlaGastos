27480c55246bd54a567677dd3c59611bd39feb8c
Lucas Nery <lucas.leite1@br.nestle.com>
2024-06-11 09:45:57 -0300

    add types

---
+++ frontend/src/types/index.tsx
@@ -0,0 +1,15 @@
+export interface Card {
+  name: string;
+  color: string
+  username: string;
+}
+
+export interface Category {
+  name: string;
+  username: string;
+}
+
+export interface People {
+  name: string;
+  username: string;
+}
