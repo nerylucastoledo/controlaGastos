5a7e9a8598f8f64421515b328c5f91db8a8baf36
Lucas Nery <lucas.leite1@br.nestle.com>
2024-06-18 11:17:35 -0300

    add toast test

---
+++ frontend/src/components/Toast/__tests__/Toast.test.tsx
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+
+import Toast from '../Toast'
+
+describe("Toast component", () => {
+  it('deveria mostrar a mensagem de "test error" na tela com a cor vermelha caso o erro seja true', () => {
+    render(<Toast message='test error' error={true} hideToast={() => {}} />)
+
+    const toastElement = screen.getByRole('alert');
+    const styles = window.getComputedStyle(toastElement);
+
+    expect(screen.getByText("test error")).toBeInTheDocument()
+    expect(styles.backgroundColor).toEqual("rgb(178, 34, 34)");
+  })
+
+  it('deveria mostrar a mensagem de "test success" na tela com a cor verde caso o erro seja false', () => {
+    render(<Toast message='test success' error={false} hideToast={() => {}} />)
+
+    const toastElement = screen.getByRole('alert');
+    const styles = window.getComputedStyle(toastElement);
+
+    expect(screen.getByText("test success")).toBeInTheDocument()
+    expect(styles.backgroundColor).toEqual("rgb(0, 100, 0)");
+  })
+})
